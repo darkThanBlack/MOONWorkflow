8af6d6e00957b507295bb5fb8c0d99910311cbe3
moonShadow <331614794@qq.com>
2024-10-17 11:01:32 +0800

    daily sync with script

--- sync.sh
+++ sync.sh
@@ -28,11 +28,31 @@ fire_sync() {
   if [[ -z $(git status -s) ]]; then
     echo "${blog_path} is clear."
   else
+    git checkout main;
+    # ensure jazzy build correct
+    xcodegen;
+    git add .;
+    git commit . -m 'daily sync with script';
+    git pull --ff;
+    git push;
+    jazzy \
+      --clean \
+      --author darkThanBlack \
+      --author_url https://darkthanblack.github.io \
+      --source-host github \
+      --source-host-url https://github.com/darkThanBlack/DTBKit \
+      --exclude "Sources/Chain/*" \
+      --output docs \
+      --theme apple;
+    mv docs ~/Documents/docs;
+    git checkout gh-pages;
+    git pull --ff;
+    mv ~/Documents/docs ./;
+    rm -rf ~/Documents/docs;
+    git add .;
+    git commit . -m 'deploy from jazzy';
+    git push;
     git checkout main
-    git add .
-    git commit . -m 'daily sync with script'
-    git pull --ff
-    git push
   fi
 }
 
@@ -41,6 +61,15 @@ fire() {
     workflow_path="/Users/xuyiding/Documents/iOS/MOONWorkflow"
     blog_path="/Users/xuyiding/Documents/iOS/darkThanBlack.github.io"
     kit_path="/Users/xuyiding/Documents/iOS/DTBKit"
+
+    # 参照 jenkins 脚本，强行指定环境变量
+    PATH=/Users/xuyiding/.rvm/gems/ruby-3.0.0/bin:/Users/xuyiding/.rvm/gems/ruby-3.0.0@global/bin:/Users/xuyiding/.rvm/rubies/ruby-3.0.0/bin:/Users/xuyiding/opt/anaconda3/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/Users/xuyiding/flutter/bin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:/Library/Apple/usr/bin:/Users/xuyiding/.pub-cache/bin:/Users/xuyiding/.rvm/bin
+
+    source /etc/profile
+    source /Users/xuyiding/.zshrc
+    # 触发 rvm 设置脚本
+    source /Users/xuyiding/.bash_profile
+
     fire_sync
   fi
 }
